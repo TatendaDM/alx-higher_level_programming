d4194b46eefba56342b646ccb370effe177d0141
TatendaDM <127248571+TatendaDM@users.noreply.github.com>
2023-08-11 06:41:20 +0200

    Update 13-insert_number.c

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -8,6 +8,7 @@
  * Return: If the function fails - NULL.
  * Otherwise - a pointer to the new node.
  */
+
 listint_t *insert_node(listint_t **head, int number)
 {
 	listint_t *node = *head, *new;
@@ -32,4 +33,3 @@ listint_t *insert_node(listint_t **head, int number)
 
 	return (new);
 }
-
