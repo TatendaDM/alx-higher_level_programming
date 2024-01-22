6b23d0a5de4897240c4fc729ea455bf228c7143a
TatendaDM <127248571+TatendaDM@users.noreply.github.com>
2024-01-22 14:39:14 +0200

    Update 0-safe_print_list.py

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python3
-
 def safe_print_list(my_list=[], x=0):
-    sum_total = 0
+    index = 0
     while True:
         try:
-            if sum_total < x:
-                print (f"{my_list[i]}", end="")
-                sum_total += 1
-            else: 
+            if index < x:
+                print(my_list[index], end='')
+                index += 1
+            else:
                 print()
-                return sum_total
+                return index
         except IndexError:
             print()
-            return sum_total
+            return index
