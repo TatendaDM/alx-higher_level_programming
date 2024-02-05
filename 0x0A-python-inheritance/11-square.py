a77fb44c4e01f8d0f284de2210422a243d227b5b
TatendaDM <127248571+TatendaDM@users.noreply.github.com>
2024-02-05 14:36:47 +0200

    Update 11-square.py

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -7,7 +7,12 @@ class Square(Rectangle):
     """class body."""
 
     def __init__(self, size):
+        '''Attributes of the class Square.
 
+        Args:
+           size (int): The size of Square.
+        '''
+        
         self.integer_validator("size", size)
         super().__init__(size, size)
         self.__size = size
